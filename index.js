17cb108a79f4e81b8d1e42b0dc06409fa4c219a3
Payal Wankhede <payalwankhede855@gmail.com>
2024-04-11 14:03:33 +0530

    Form created

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const urlRoute = require("./routers/url");
 const { connectToMongoDB } = require("./connection");
+const path = require("path");
 const URL = require("./models/url");
+const staticRouter = require("./routers/staticRouter");
 const app = express();
 const PORT = 8001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+connectToMongoDB("mongodb://0.0.0.0:27017/short-url").then(() =>
+  console.log("MongoDB Connected")
+);
+
+app.get("/url", async (req, res) => {
+  const allURL = await URL.find({});
+  return res.render("home", {
+    urls: allURL,
+  });
+});
+
 app.use("/url", urlRoute);
-app.get("/:shortId", async (req, res) => {
+app.use("/", staticRouter);
+
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
@@ -23,11 +43,5 @@ app.get("/:shortId", async (req, res) => {
   );
   res.redirect(entry.redirectURL);
 });
-
-
-
-connectToMongoDB("mongodb://0.0.0.0:27017/short-url").then(() =>
-  console.log("MongoDB Connected")
-);
 
 app.listen(PORT, () => console.log(`Server strted at PORT ${PORT}`));
